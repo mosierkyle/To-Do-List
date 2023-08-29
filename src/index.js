145f27d0f716750098627a4417bf80b57dce8d51
Kyle Mosier <mosierkyle@gmail.com>
2023-08-28 23:29:28 -0700

    Clean up the index.js page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { toDoItem, projectToDoItem } from "./Modules/create-ToDo-item";
-import { toDoList,  } from "./Modules/create-ToDo-list";
-import { projects, lists, week} from "./Modules/miscObjs";
-import { getLists } from "./Modules/getLists";
+import { toDoList } from "./Modules/create-ToDo-list";
+import { projects, lists} from "./Modules/miscObjs";
 
 const project1 = new toDoList('school')
 const project2 = new toDoList('gym')
@@ -11,9 +10,7 @@ projects.addItem(project2)
 
 const toDo1 = new toDoItem('take trash out', 7 , 'medium', project1, true);
 
-// console.log(toDo1.getLists());
 toDo1.addTo()
-
 console.log(lists.lists[0]);
 console.log(lists.lists[1]);
 console.log(projects.projectList[0]);
